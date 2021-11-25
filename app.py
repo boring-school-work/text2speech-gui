763b981fc2937d20e6ff58d524200c62c3a1117b
David Saah <dasorange.hope@gmail.com>
2021-11-25 21:34:15 +0000

    testing sound in PyQt5

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 import sys
+from gtts import gTTS
+from playsound import playsound
 
 
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
-        self.button_checked = True
+        self.checked = True
         self.setWindowTitle("Python Text To Speech")
         self.button = QPushButton('Click Me!')
-
-        # if a widget does not provide a signal that sends the current state, you need to retrive the value from the widget directly in your handler
-        self.button.setCheckable(True)
-        self.button.released.connect(self.on_release)
-        self.button.setChecked(self.button_checked)
         self.setCentralWidget(self.button)
-    
-    def on_release(self):
-        print(self.button.isChecked())
-        self.button_checked = self.button.isChecked()
-        print(self.button_checked)
+        self.button.setCheckable(True)
+        self.button.setChecked(self.checked)
+        self.button.clicked.connect(self.on_toggle)
+
+    def on_toggle(self):
+        self.tts = gTTS(text='clicked!', lang="en")
+        self.tts.save("output.mp3")
+        playsound('output.mp3')
+        
         
         
 
