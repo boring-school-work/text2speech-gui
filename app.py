0698d3d53dcb17fbe020eccfe64026e9b632f398
David Saah <dasorange.hope@gmail.com>
2021-11-25 22:18:04 +0000

    add initial solution for text2speech-gui

--- app.py
+++ app.py
@@ -1,43 +1,40 @@
-from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtCore import QSize
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget, QPushButton
 import sys
-
-
-window_titles = [
-    "MyApp",
-    "MyApp2",
-    "MyApp3",
-    "MyApp4",
-    "MyApp5",
-    "MyApp6",
-    "Something went wrong"
-]
+from gtts import gTTS
+from playsound import playsound
 
 
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
-        self.n_times_clicked = 0
-        self.setWindowTitle(window_titles[0])
-        self.button = QPushButton("Click me")
-        self.button.clicked.connect(self.on_button_clicked)
-        self.setCentralWidget(self.button)
-        self.windowTitleChanged.connect(self.on_window_title_changed)
+        self.setWindowTitle('Python Text To Speech')
+        self.setMinimumSize(QSize(500, 500))
 
-    def on_button_clicked(self):
-        self.n_times_clicked += 1
-        new_window_title = window_titles[self.n_times_clicked]
-        self.setWindowTitle(new_window_title)
+        self.button = QPushButton('Print')
+        self.label = QLabel()
+        self.input = QLineEdit()
+        self.button.setCheckable(True)
+        self.button.clicked.connect(self.on_click)
+        # self.input.textChanged.connect(self.label.setText)
 
-    def on_window_title_changed(self, title):
-        print(f"Window title changed to {title}")
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.button)
+        layout.addWidget(self.label)
 
-        if title == 'Something went wrong':
-            self.button.setDisabled(True)
-        
-        
-        
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
+    def on_click(self):
+        self.text = self.input.text()
+        tts = gTTS(text=self.text, lang='en')
+        tts.save('output.mp3')
+        self.label.setText(self.text)
+        playsound('output.mp3')
 
 
 
