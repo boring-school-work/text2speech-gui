80d1358ac7405fcb87431d3a276786c7bba747f2
David Saah <dasorange.hope@gmail.com>
2021-12-04 23:17:30 +0000

    working with layout and images

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from PyQt5.QtCore import QSize
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget, QPushButton, QTextEdit
+from os import truncate
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QLabel, QVBoxLayout, QWidget, QPushButton, QTextEdit
 import sys
 from gtts import gTTS
 from playsound import playsound
@@ -11,23 +13,51 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle('Python Text To Speech')
         self.setMinimumSize(QSize(500, 500))
+        align_top_center = Qt.AlignTop | Qt.AlignHCenter
 
-        self.setMouseTracking(True)
+        self.button = QPushButton('Convert Text To Speech')
+        self.button.clicked.connect(self.on_click)
+        self.button.setCheckable(True)
 
-        self.label = QLabel('Click in this window')
-        self.setCentralWidget(self.label)
+        self.input = QTextEdit()
+        self.input.setFixedSize(QSize(500, 500))
+        self.input.setPlaceholderText('Enter text here')
 
-    def mouseMoveEvent(self, e):
-        self.label.setText('mouseMoveEvent')
+        self.title = QLabel('Welcome To Python Text To Speech')
+        titleFont = self.title.font()
+        titleFont.setPointSize(20)
+        self.title.setFont(titleFont)
+        self.title.setAlignment(align_top_center)
 
-    def mousePressEvent(self, e):
-        self.label.setText('mousePressEvent')
-    
-    def mouseReleaseEvent(self, e):
-        self.label.setText('mouseReleaseEvent')
-    
-    def mouseDoubleClickEvent(self, e):
-        self.label.setText('mouseDoubleClickEvent')
+        self.label = QLabel('This Application is used to convert text to speech. This proves useful for people who are blind to be able to read text to promote learning thus satifying the second AU Goal which states: "Well educated citizens and skills revolution underpinned by Science, Technology and Innovation."')
+        self.label.setWordWrap(True)
+        labelFont = self.label.font()
+        labelFont.setPointSize(12)
+        self.label.setFont(labelFont)
+        self.label.setAlignment(align_top_center)
+
+        self.image = QLabel()
+        self.image.setPixmap(QPixmap('tts.png'))
+        self.image.setAlignment(Qt.AlignTop)
+
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.title)
+        layout.addWidget(self.image)
+        layout.addWidget(self.label)
+        layout.addWidget(self.input)
+        layout.addWidget(self.button)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
+    def on_click(self):
+        self.text = self.input.toPlainText()
+        tts = gTTS(text=self.text, lang='en')
+        tts.save('output.mp3')
+        playsound('output.mp3')
 
 
 
