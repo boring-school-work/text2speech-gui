9db6118cc16ff8c7b4f47fe5015e07a10d35fe69
hesed <hesed@pop-os.localdomain>
2021-12-06 13:57:10 +0000

    testing nesting layouts

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from os import truncate
 from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QLabel, QVBoxLayout, QWidget, QPushButton, QTextEdit
+from PyQt5.QtWidgets import QApplication, QHBoxLayout, QLabel, QMainWindow, QLabel, QVBoxLayout, QWidget, QPushButton, QTextEdit
 import sys
 from gtts import gTTS
 from playsound import playsound
@@ -13,43 +12,50 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle('Python Text To Speech')
         self.setMinimumSize(QSize(500, 500))
-        align_top_center = Qt.AlignTop | Qt.AlignHCenter
 
         self.button = QPushButton('Convert Text To Speech')
         self.button.clicked.connect(self.on_click)
         self.button.setCheckable(True)
 
         self.input = QTextEdit()
-        self.input.setFixedSize(QSize(500, 500))
+        self.input.setFixedSize(QSize(600, 600))
         self.input.setPlaceholderText('Enter text here')
 
         self.title = QLabel('Welcome To Python Text To Speech')
         titleFont = self.title.font()
         titleFont.setPointSize(20)
         self.title.setFont(titleFont)
-        self.title.setAlignment(align_top_center)
+        self.title.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
 
         self.label = QLabel('This Application is used to convert text to speech. This proves useful for people who are blind to be able to read text to promote learning thus satifying the second AU Goal which states: "Well educated citizens and skills revolution underpinned by Science, Technology and Innovation."')
         self.label.setWordWrap(True)
         labelFont = self.label.font()
         labelFont.setPointSize(12)
         self.label.setFont(labelFont)
-        self.label.setAlignment(align_top_center)
+        self.label.setAlignment(Qt.AlignTop)
 
         self.image = QLabel()
-        self.image.setPixmap(QPixmap('tts.png'))
+        self.image.setPixmap(QPixmap('logo.png'))
         self.image.setAlignment(Qt.AlignTop)
 
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.title)
-        layout.addWidget(self.image)
-        layout.addWidget(self.label)
-        layout.addWidget(self.input)
-        layout.addWidget(self.button)
+        V_layout = QVBoxLayout()
+        H_layout = QHBoxLayout()
+        V_layout.setContentsMargins(0, 0, 0, 0)
+        H_layout.setContentsMargins(0, 0, 0, 0)
+        H_layout.setSpacing(0)
+
+        H_layout.addWidget(self.image)
+        H_layout.addWidget(self.label)
+
+
+        V_layout.addWidget(self.title)
+        V_layout.addLayout(H_layout)
+        V_layout.addWidget(self.input)
+        V_layout.addWidget(self.button)
 
         container = QWidget()
-        container.setLayout(layout)
+        container.setLayout(V_layout)
 
         self.setCentralWidget(container)
 
