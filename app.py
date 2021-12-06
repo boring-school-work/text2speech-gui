81a12c8b93d2727b406c619d4a7edcfd72590564
David Saah <dasorange.hope@gmail.com>
2021-12-06 15:48:43 +0000

    align image and text to center

--- app.py
+++ app.py
@@ -16,9 +16,10 @@ class MainWindow(QMainWindow):
         self.button = QPushButton('Convert Text To Speech')
         self.button.clicked.connect(self.on_click)
         self.button.setCheckable(True)
+        self.button.setMinimumSize(QSize(600, 35))
 
         self.input = QTextEdit()
-        self.input.setFixedSize(QSize(600, 600))
+        self.input.setMinimumSize(QSize(600, 600))
         self.input.setPlaceholderText('Enter text here')
 
         self.title = QLabel('Welcome To Python Text To Speech')
@@ -27,27 +28,28 @@ class MainWindow(QMainWindow):
         self.title.setFont(titleFont)
         self.title.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
 
-        self.label = QLabel('This Application is used to convert text to speech. This proves useful for people who are blind to be able to read text to promote learning thus satifying the second AU Goal which states: "Well educated citizens and skills revolution underpinned by Science, Technology and Innovation."')
-        self.label.setWordWrap(True)
-        labelFont = self.label.font()
-        labelFont.setPointSize(12)
-        self.label.setFont(labelFont)
-        self.label.setAlignment(Qt.AlignTop)
+        self.label = QLabel(self)
+        self.image = QPixmap('logo.png')
+        self.label.setPixmap(self.image)
 
-        self.image = QLabel()
-        self.image.setPixmap(QPixmap('logo.png'))
-        self.image.setAlignment(Qt.AlignTop)
+        self.text_label = QLabel('This Application is used to convert text to speech. This proves useful for people who are blind to be able to read text to promote learning thus satifying the second AU Goal which states: "Well educated citizens and skills revolution underpinned by Science, Technology and Innovation."', self)
+        self.text_label.setWordWrap(True)
+        labelFont = self.text_label.font()
+        labelFont.setPointSize(12)
+        self.text_label.setFont(labelFont)
+        self.text_label.setMinimumHeight(self.image.height())
+        self.text_label.setStyleSheet('''
+        padding: 0px 10px 0px 10px;
+        ''')
 
 
         V_layout = QVBoxLayout()
         H_layout = QHBoxLayout()
-        V_layout.setContentsMargins(0, 0, 0, 0)
-        H_layout.setContentsMargins(0, 0, 0, 0)
-        H_layout.setSpacing(0)
 
-        H_layout.addWidget(self.image)
+        H_layout.setAlignment(Qt.AlignCenter)
+
         H_layout.addWidget(self.label)
-
+        H_layout.addWidget(self.text_label)        
 
         V_layout.addWidget(self.title)
         V_layout.addLayout(H_layout)
