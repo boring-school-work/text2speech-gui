4f5c67f04d577b974569cd530703e8eac277fdd6
David Saah <dasorange.hope@gmail.com>
2021-12-08 01:16:38 +0000

    add convert pdf and/or docx to speech

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QGridLayout, QHBoxLayout, QMainWindow, QLabel, QVBoxLayout, QWidget, QPushButton, QTextEdit
+from PyQt5.QtWidgets import QApplication, QGridLayout, QHBoxLayout, QMainWindow, QLabel, QVBoxLayout, QWidget, QPushButton, QTextEdit, QFileDialog, QMessageBox
 import sys
 from gtts import gTTS
 from playsound import playsound
+import docx2txt
+from pdf2docx import parse
 
 
 class MainWindow(QMainWindow):
@@ -49,14 +51,24 @@ class MainWindow(QMainWindow):
 
         self.choose_file_button = QPushButton("Select file")
         self.choose_file_button.setCheckable(True)
-        self.choose_file_button.clicked.connect(self.on_file_button_click)
+        self.choose_file_button.clicked.connect(self.openFileNameDialog)
         self.choose_file_button.setFixedSize(QSize(100, 35))
+
+        self.file_label_data = QLabel("")
+        file_label_data_font = self.file_label_data.font()
+        file_label_data_font.setPointSize(12)
+        self.file_label_data.setFont(file_label_data_font)
+
+        self.play_button = QPushButton("Play")
+        self.play_button.setCheckable(True)
+        self.play_button.setFixedSize(QSize(100, 35))
+        self.play_button.clicked.connect(self.play_audio)
 
         layout = QVBoxLayout()
         V_layout_1 = QVBoxLayout()
         H_layout_1 = QHBoxLayout()
         H_layout_2 = QHBoxLayout()
-        grid = QGridLayout()
+        self.grid = QGridLayout()
 
         H_layout_1.setAlignment(Qt.AlignCenter)
         H_layout_1.addWidget(self.image_label)
@@ -65,11 +77,13 @@ class MainWindow(QMainWindow):
         V_layout_1.addWidget(self.text_input, 1)
         V_layout_1.addWidget(self.convert_input_button)
 
-        grid.addWidget(self.file_label, 0, 0, 1, 1, Qt.AlignHCenter)
-        grid.addWidget(self.choose_file_button, 0, 0, 2, 1, Qt.AlignCenter)
+        self.grid.addWidget(self.file_label, 0, 0, 1, 2, Qt.AlignCenter)
+        self.grid.addWidget(self.choose_file_button, 1, 0, 1, 1, Qt.AlignCenter)
+        self.grid.addWidget(self.play_button, 1, 1, 1, 1, Qt.AlignCenter)
+        self.grid.addWidget(self.file_label_data, 2, 0, 1, 2, Qt.AlignCenter)
 
         H_layout_2.addLayout(V_layout_1)
-        H_layout_2.addLayout(grid)
+        H_layout_2.addLayout(self.grid)
      
         layout.addWidget(self.title)
         layout.addLayout(H_layout_1)
@@ -81,16 +95,45 @@ class MainWindow(QMainWindow):
 
         self.setCentralWidget(container)
 
+
     def on_input_button_click(self):
         self.text = self.text_input.toPlainText()
         tts = gTTS(text=self.text, lang='en')
         tts.save('output.mp3')
         playsound('output.mp3')
 
-    def on_file_button_click(self):
-        print(self.size())
-        x, y = self.size().width(), self.size().height()
-        print(x, y)
+
+    def convert_docx_to_speech(self, file_name):
+        tts = gTTS(text=docx2txt.process(file_name), lang='en')
+        tts.save(file_name.replace('.docx', '.mp3'))
+
+
+    def convert_pdf_to_speech(self, file_name):
+        doc_file_path = file_name.replace('.pdf', '.docx')
+        parse(file_name, doc_file_path)
+        tts = gTTS(text=docx2txt.process(doc_file_path), lang='en')
+        tts.save(file_name.replace('.pdf', '.mp3'))
+
+
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        self.fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "","All Files (*);;Word Document (*.doc);;Pdf Document (*.pdf)", options=options)
+        if self.fileName:
+            if self.fileName.endswith('.docx'):
+                self.file_label_data.setText(f'Converting {self.fileName} to speech...')
+                self.convert_docx_to_speech(self.fileName)
+                self.file_label_data.setText(f'Converted {self.fileName} to speech\nYou can play the audio file by clicking on play button')
+            elif self.fileName.endswith('.pdf'):
+                self.file_label_data.setText(f'Converting {self.fileName} to speech...')
+                self.convert_pdf_to_speech(self.fileName)
+                self.file_label_data.setText(f'Converted {self.fileName} to speech\nYou can play the audio file by clicking on play button')
+
+    def play_audio(self):
+        if self.fileName.endswith('.docx'):
+            playsound(self.fileName.replace('.docx', '.mp3'))
+        elif self.fileName.endswith('.pdf'):
+            playsound(self.fileName.replace('.pdf', '.mp3'))       
 
 
 
